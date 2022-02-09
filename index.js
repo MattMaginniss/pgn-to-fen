422e37633f76803d5e9773895cba74cc3a615bc2
Matthew Maginniss <Matthew.Maginniss@gmail.com>
2022-02-09 17:47:06 -0500

    Set base functionality

---
+++ index.js
@@ -0,0 +1,26 @@
+fenPositions = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+chessBoardArray = [
+  'R','N','B','Q','K','B','N','R',
+  'P','P','P','P','P','P','P','P',
+  '1','1','1','1','1','1','1','1',
+  '1','1','1','1','1','1','1','1',
+  '1','1','1','1','1','1','1','1',
+  '1','1','1','1','1','1','1','1',
+  'p','p','p','p','p','p','p','p',
+  'r','n','b','q','k','b','n','r']
+whiteMove = true
+encroissantSquare = '-'
+castlingRights = 'KQkq'
+halfMoves = 0
+moveNumber = 0
+lastMove = 'First move'
+
+function printFEN() {
+  return fenPositions + ' ' + (whiteMove ? 'w' : 'b') + ' ' + castlingRights + ' ' + encroissantSquare  + ' ' + halfMoves + ' ' + moveNumber
+}
+
+function getFENpositions() {
+  // TODO: will calculate the fenPositions string from the chessBoardArray
+}
+
+console.log(printFEN())
